89a95d1f43604a0f61f6e8d9896916ed8bae9e9d
divisiongroup <divisiongroup@gmail.com>
2018-10-27 22:22:18 -0700

    division group theme updates

--- header.php
+++ header.php
@@ -2,22 +2,20 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1">
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div id="wrapper" class="hfeed">
-<header id="header" role="banner">
-<section id="branding">
-<div id="site-title"><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '<h1>'; } ?><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_html( get_bloginfo( 'name' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a><?php if ( is_front_page() || is_home() || is_front_page() && is_home() ) { echo '</h1>'; } ?></div>
-<div id="site-description"><?php bloginfo( 'description' ); ?></div>
-</section>
-<nav id="menu" role="navigation">
-<!-- <div id="search">
-<?php get_search_form(); ?>
-</div> -->
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
-</nav>
+<div id="wrapper">
+<header class="header" role="banner">
+		<div class="logo">
+			<?php echo csl_CustomSiteLogo_show_logo(); ?>
+			<div id="site-description"><?php bloginfo( 'description' ); ?></div>
+		</div>
+		<nav id="menu" role="navigation">
+		<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+		</nav>
 </header>
 <div id="container">
