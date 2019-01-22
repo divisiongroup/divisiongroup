719da74a2ca2cf5939afe27c611680498f14c1e1
divisiongroup <divisiongroup@Garrets-MacBook-Pro.local>
2019-01-22 15:43:46 -0800

    script/app.js update - menu, mobile icon css update

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,56 @@
 (function($){
 	$(document).foundation();
 	//js code here
+	$ = jQuery;
+
+    // window scroll function
+    $(window).scroll(function() {
+
+    	var winWidth = $(window).width();
+
+    	if(winWidth >= 1024){
+
+    		var scroll = $(window).scrollTop();
+			var target = $("#poop").offset();
+
+	        if (scroll >= target.top - 200) {
+
+	            $("#menu").css({"display": 'none'});
+	            $(".menu-icon").fadeIn( "fast" );
+
+	        } else {
+
+	        	$("#menu").fadeIn( "fast" );
+	            $(".menu-icon").css({"display": 'none'});
+
+	        }
+    	}
+    });
+
+    // window resize function
+    var id;
+	$(window).resize(function() {
+	    clearTimeout(id);
+	    id = setTimeout(doneResizing, 500);
+	    
+	});
+
+	function doneResizing(){
+
+	  var winWidth = $(window).width();
+
+	  if(winWidth >= 1024){
+
+	  	$("#menu").fadeIn( "fast" );
+	  	$(".menu-icon").css({"display": 'none'});
+
+	  } else {
+
+	  	$("#menu").css({"display": 'none'});
+	  	$(".menu-icon").fadeIn( "fast" );
+
+	  }
+
+	}
+
 })(jQuery);
