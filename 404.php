89cf49ec8be553d4e7e85705400310914378bee1
divisiongroup <divisiongroup@gmail.com>
2020-05-24 18:23:06 -0700

    app updates

--- 404.php
+++ 404.php
@@ -1,9 +1,5 @@
-<?php get_header(); ?>
-<section id="content" role="main">
-<article id="post-0" class="post not-found">
-<section class="entry-content">
-</section>
-</article>
-</section>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: ".get_bloginfo('url'));
+exit();
+?>
--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
 add_action( 'after_setup_theme', 'divisiongroup_setup' );
 function divisiongroup_setup()
 {
@@ -12,6 +14,11 @@ register_nav_menus(
 array( 'main-menu' => __( 'Main Menu', 'divisiongroup' ) )
 );
 }
+
+//Hide WordPress Version Number from Generator Meta Tag
+remove_action('wp_head', 'wp_generator');
+// END ENQUEUE PARENT ACTION
+
 add_action( 'wp_enqueue_scripts', 'my_custom_script_load', 1000 );
 function my_custom_script_load(){
 	wp_enqueue_style( 'override-style', get_stylesheet_directory_uri() . '/css/app.css', array( 'style' ) );
@@ -21,6 +28,31 @@ function my_custom_script_load(){
   	wp_enqueue_script( 'foundation', get_stylesheet_directory_uri() . '/scripts/foundation.min.js', array( 'jquery' ), '', true );
   	wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/scripts/app.js', array( 'jquery' ),'', true );
 }
+
+//Defer CSS
+function add_rel_preload($html, $handle, $href, $media) {
+    
+    if (is_admin())
+        return $html;
+
+     $html = <<<EOT
+<link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' />
+EOT;
+    return $html;
+}
+add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
+
+//Defer JavaScript
+function defer_parsing_of_js( $url ) {
+    if ( is_user_logged_in() ) return $url; //don't break WP Admin
+    if ( FALSE === strpos( $url, '.js' ) ) return $url;
+    if ( strpos( $url, 'jquery.js' ) ) return $url;
+    return str_replace( ' src', ' defer src', $url );
+}
+add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
+
+
+
 add_action( 'comment_form_before', 'divisiongroup_enqueue_comment_reply_script' );
 function divisiongroup_enqueue_comment_reply_script()
 {
