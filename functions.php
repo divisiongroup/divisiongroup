c30b17358cfdb636094eb966482da5864fdad3c2
divisiongroup <divisiongroup@gmail.com>
2020-10-19 19:34:21 -0700

    dg app updates

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php
 // Exit if accessed directly
 if ( !defined( 'ABSPATH' ) ) exit;
+
 add_action( 'after_setup_theme', 'divisiongroup_setup' );
 function divisiongroup_setup()
 {
@@ -29,18 +30,6 @@ function my_custom_script_load(){
   	wp_enqueue_script( 'app', get_stylesheet_directory_uri() . '/scripts/app.js', array( 'jquery' ),'', true );
 }
 
-//Defer CSS
-function add_rel_preload($html, $handle, $href, $media) {
-    
-    if (is_admin())
-        return $html;
-
-     $html = <<<EOT
-<link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' />
-EOT;
-    return $html;
-}
-add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
 
 //Defer JavaScript
 function defer_parsing_of_js( $url ) {
@@ -53,51 +42,5 @@ add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
 
 
 
-add_action( 'comment_form_before', 'divisiongroup_enqueue_comment_reply_script' );
-function divisiongroup_enqueue_comment_reply_script()
-{
-if ( get_option( 'thread_comments' ) ) { wp_enqueue_script( 'comment-reply' ); }
-}
-add_filter( 'the_title', 'divisiongroup_title' );
-function divisiongroup_title( $title ) {
-if ( $title == '' ) {
-return '&rarr;';
-} else {
-return $title;
-}
-}
-add_filter( 'wp_title', 'divisiongroup_filter_wp_title' );
-function divisiongroup_filter_wp_title( $title )
-{
-return $title . esc_attr( get_bloginfo( 'name' ) );
-}
-add_action( 'widgets_init', 'divisiongroup_widgets_init' );
-function divisiongroup_widgets_init()
-{
-register_sidebar( array (
-'name' => __( 'Sidebar Widget Area', 'divisiongroup' ),
-'id' => 'primary-widget-area',
-'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
-'after_widget' => "</li>",
-'before_title' => '<h3 class="widget-title">',
-'after_title' => '</h3>',
-) );
-}
-function divisiongroup_custom_pings( $comment )
-{
-$GLOBALS['comment'] = $comment;
-?>
-<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo comment_author_link(); ?></li>
-<?php 
-}
-add_filter( 'get_comments_number', 'divisiongroup_comments_number' );
-function divisiongroup_comments_number( $count )
-{
-if ( !is_admin() ) {
-global $id;
-$comments_by_type = &separate_comments( get_comments( 'status=approve&post_id=' . $id ) );
-return count( $comments_by_type['comment'] );
-} else {
-return $count;
-}
-}
+
+
