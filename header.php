3e27e2c2ac813545890d756c5ec019044c2227cf
divisiongroup <divisiongroup@gmail.com>
2021-01-10 03:38:48 -0800

    Header & Footer updates

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+<link rel="stylesheet" href="https://use.typekit.net/vyr8bjx.css">
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1">
